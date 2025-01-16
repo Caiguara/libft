15824ba327ce009f32857fad4cb928872a1b663d
Misael Ignacio Caiguara <mignacio@c2r15s6.42madrid.com>
2025-01-16 16:02:11 +0100

    modified ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:41:29 by mignacio          #+#    #+#             */
-/*   Updated: 2025/01/15 16:41:29 by mignacio         ###   ########.fr       */
+/*   Updated: 2025/01/16 16:00:03 by mignacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	len;
+	size_t	j;
+	size_t	src_len;
+    size_t  dst_len;
 
-	len = ft_strlen(src);
-	i = 0;
+    src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	j = 0;
+	if (size == 0)
+		return (dst_len + src_len);
+	if (size <= dst_len)
+    	return (dst_len + src_len);
+	while ((src[j] != '\0') && (dst_len +j < size - 1))
+	{
+		dst[dst_len + j] = src[j];
+		j++;
+	}
+	dst[dst_len + j] = '\0';
+	return (dst_len + src_len);
 }
