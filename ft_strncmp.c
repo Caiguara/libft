e1083276e8ef7d3b66b199dcb17bf9a6c6313b82
Misael Ignacio Caiguara <mignacio@c2r7s6.42madrid.com>
2025-01-19 15:07:18 +0100

    modified strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 16:37:04 by mignacio          #+#    #+#             */
-/*   Updated: 2025/01/16 17:24:01 by mignacio         ###   ########.fr       */
+/*   Updated: 2025/01/19 15:06:21 by mignacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (i < n && *s1 && *s2)
+	while (i < n && (s1[i] || s2[i]))
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if (i == n)
-		return (0);
-	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+	return (0);
 }
