a51b230d597d00b8f7f97bbfd5f71ab213574e7d
Misael Ignacio Caiguara <mignacio@c2r14s1.42madrid.com>
2025-01-22 15:36:45 +0100

    added strchr

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,13 +6,46 @@
 /*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 18:31:38 by mignacio          #+#    #+#             */
-/*   Updated: 2025/01/22 13:40:24 by mignacio         ###   ########.fr       */
+/*   Updated: 2025/01/22 14:25:35 by mignacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
-char	*ft_strdup(const char *s)
+size_t	ft_strlen(const char *str)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *str)
+{
+	size_t	i;
+	size_t	len;
+	const char	*result;
+
+	len = ft_strlen(str);
+	while (str[i] != '\0')
+	{
+		result[i] = str[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+int	main(void)
+{
+	char	*origin = "abcdefg";
+	char	*result;
+
+	result = strdup(origin);
+	printf("%s\n", result);
+	return (0);
 }
