6d33e3b9bbf63578a7a4c69ad773b05460fb6bfb
Misael Ignacio Caiguara <mignacio@c2r5s6.42madrid.com>
2025-01-23 17:44:41 +0100

    modified memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,6 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 17:05:47 by mignacio          #+#    #+#             */
+/*   Updated: 2025/01/23 17:44:11 by mignacio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	unsigned char		*dest_ptr;
+	const unsigned char	*src_ptr;
+
+	dest_ptr = (unsigned char *) dest;
+	src_ptr = (const unsigned char *) src;
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	while (i < n)
+	{
+		dest_ptr[i] = src_ptr[i];
+		i++;
+	}
+	return (dest);
 }
