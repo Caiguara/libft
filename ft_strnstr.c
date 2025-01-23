94d9afe81acdc6593b27aea2b6a5da04cd6f0d08
Misael Ignacio Caiguara <mignacio@c2r5s6.42madrid.com>
2025-01-23 13:20:16 +0100

    added strnstr and modified libft.h

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 16:16:52 by mignacio          #+#    #+#             */
+/*   Updated: 2025/01/23 13:15:48 by mignacio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (little[0] == '\0')
+		return ((char *) big);
+	while (*big && i < len)
+	{
+		if (big[i] == little[0])
+		{
+			j = 0;
+			while (big[i + j] != '\0' && little[j] != '\0'
+				&& big[i + j] == little[j] && (i + j) < len)
+				j++;
+			if (little[j] == '\0')
+			{
+				return ((char *)&big[i]);
+			}
+		}
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 13:41:12 by mignacio          #+#    #+#             */
-/*   Updated: 2025/01/22 13:32:10 by mignacio         ###   ########.fr       */
+/*   Updated: 2025/01/23 13:19:49 by mignacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_isdigit(int c);
-int		ft_isalpha(int c);
+int		ft_atoi(const char *nptr);
 int		ft_isalnum(int c);
+int		ft_isalpha(int c);
 int		ft_isascii(int c);
+int		ft_isdigit(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *str, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-int		ft_atoi(const char *nptr);
 #endif
