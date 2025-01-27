0ad556394273788a9ee5f52c7e3e7fcf6c114edf
Misael (42) <mignacio@student.42madrid.com>
2025-01-27 19:24:41 +0100

    added putnbr

---
+++ ft_putendl_fd.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 17:45:35 by mignacio          #+#    #+#             */
+/*   Updated: 2025/01/27 18:22:35 by mignacio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
