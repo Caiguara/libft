d6c363ab9eb3b560f6fb2bd5d6a2c2b317ba6ef7
Misael (42) <mignacio@student.42madrid.com>
2025-02-02 20:22:17 +0100

    added lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 19:47:06 by mignacio          #+#    #+#             */
+/*   Updated: 2025/02/02 20:21:46 by mignacio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *) malloc (sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new -> content = content;
+	new -> next = NULL;
+	return (new);
+}
