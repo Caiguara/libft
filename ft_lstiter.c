02ebdc487e43d090d7e599e9bbfe80d223e264cc
Misael (42) <mignacio@student.42madrid.com>
2025-02-06 15:15:52 +0100

    added lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,16 +6,16 @@
 /*   By: mignacio <mignacio@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:55:51 by mignacio          #+#    #+#             */
-/*   Updated: 2025/02/06 15:10:54 by mignacio         ###   ########.fr       */
+/*   Updated: 2025/02/06 15:15:38 by mignacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void(*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst || !f)
-		return ;	
+		return ;
 	while (lst)
 	{
 		f(lst -> content);
